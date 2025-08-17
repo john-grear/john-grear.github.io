d5e139ed8a910494f2d0aacee6da49273c4c9e5a
John <piquagrearj@gmail.com>
2025-08-16 22:09:58 -0400

    Changed primary color.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 
 import { createApp } from 'vue';
@@ -13,10 +14,34 @@ import { router } from './router';
 
 const app = createApp(App);
 
+const themePreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: 'var(--new-primary-50)',
+      100: 'var(--new-primary-100)',
+      200: 'var(--new-primary-200)',
+      300: 'var(--new-primary-300)',
+      400: 'var(--new-primary-400)',
+      500: 'var(--new-primary-500)',
+      600: 'var(--new-primary-600)',
+      700: 'var(--new-primary-700)',
+      800: 'var(--new-primary-800)',
+      900: 'var(--new-primary-900)',
+      950: 'var(--new-primary-950)',
+    },
+    // color: {
+    //   text: {
+    //     surface: 'var(--p-surface-900)',
+    //     primary: 'var(--p-primary-500)',
+    //   },
+    // },
+  },
+});
+
 app.use(PrimeVue, {
   ripple: true,
   theme: {
-    preset: Aura,
+    preset: themePreset,
     options: {
       darkModeSelector: '.dark-mode',
       cssLayer: false,
