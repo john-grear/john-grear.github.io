080674098554e6a0780b642fed16941b2fea4dde
John <piquagrearj@gmail.com>
2025-12-26 14:58:57 -0500

    Stop spacebar from scrolling page.

--- src/megaman/components/MegaMan.vue
+++ src/megaman/components/MegaMan.vue
@@ -5,6 +5,22 @@
 
   onMounted(start);
   onUnmounted(stop);
+
+  // Stop spacebar from scrolling page
+  window.addEventListener('keydown', function (e) {
+    // Check if the pressed key is the spacebar
+    if (e.key !== ' ') return;
+
+    // Check there is a target
+    const target = e.target as HTMLElement | null;
+    if (!target) return;
+
+    // Check if the event target is not an input, textarea, or a content-editable element
+    if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable)
+      return;
+
+    e.preventDefault();
+  });
 </script>
 
 <template>
