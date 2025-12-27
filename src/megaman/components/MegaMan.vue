e562022adbb923272d3a08da7807df9ffdfb5a35
John <piquagrearj@gmail.com>
2025-12-27 08:11:52 -0500

    Added comment to explain reasoning for raf more.

--- src/megaman/components/MegaMan.vue
+++ src/megaman/components/MegaMan.vue
@@ -5,6 +5,7 @@
 
   // Slow down the start function to let styling position everything first
   // Removing RAF's breaks the positioning of everything except MegaMan
+  // Only an issue because spawn is using margin instead of a fixed position from absolute and top
   onMounted(() => {
     requestAnimationFrame(() => requestAnimationFrame(() => requestAnimationFrame(start)));
   });
